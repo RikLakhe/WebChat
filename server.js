b8412b04b4ec04e722a7a2e4d18bf1c96f6065f3
riklakhe <rikeshlakhe@gmail.com>
2017-09-13 00:28:10 +0545

    part6 done complete

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ io.sockets.on('connection', (socket) => {
 
     //disconnect
     socket.on('disconnect', (data) => {
+        // if(!socket.username)return;
+        users.splice(users.indexOf(socket.username), 1);
+        updataUsername();
         connections.splice(connections.indexOf(socket), 1);
         console.log('Connected : %s sockets connected', connections.length);
     });
@@ -33,8 +36,20 @@ io.sockets.on('connection', (socket) => {
     socket.on('send message', (data) => {
         // checking the message being sent
         // console.log(data);
-        io.sockets.emit('new message', { msg: data });
+        io.sockets.emit('new message', { msg: data, user: socket.username });
 
     });
 
+    // new user
+    socket.on('new user', (data, callback) => {
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updataUsername();
+    })
+
+
+    function updataUsername() {
+        io.sockets.emit('get users', users);
+    }
 });
